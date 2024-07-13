9a11906375abc6b33f0a16de3e7b3fae02d88fc9
Ankit <ankitroaster@gmail.com>
2024-07-13 14:29:09 +0530

    Added Opening page and routes for Navigation

---
+++ src/pages/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Opening from '../components/Jobs/Openings/Opening'
+
+function Jobs() {
+  return (
+    <>
+    <Opening/>
+    </>
+  )
+}
+
+export default Jobs
